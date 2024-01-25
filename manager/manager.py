def102606d80504750180ea5e971f4f656aab388
UnknwnDev <0unknwn.dev@gmail.com>
2024-01-24 19:12:43 -0800

    [Feature] Manager initialization

--- manager/manager.py
+++ manager/manager.py
@@ -1,7 +1,39 @@
 #!/usr/bin/env python3.11 
+from os import listdir
+from todo import TodoList
+
 
 # This file will manage the calls creation, loading, and saving of tasks. Nothing else
 
 class Manager:
     def __init__(self) -> None:
-        pass
+        self.TodoList = {}
+
+    def start(self):
+        '''The function "start" initializes a dictionary called "TodoList" with instances of the "TodoList"
+        class, using names obtained from a JSON file.
+        
+        '''
+        names = self.__get_jsons_names()
+        for name in names:
+            self.TodoList[name] = TodoList(name)
+
+    def __get_jsons_names(self):
+        '''The function `__get_jsons_names` retrieves the names of all JSON files in the "./docs"
+        directory.
+        
+        '''
+        path = "./docs"
+        files = listdir(path)
+        names = []
+        for file in files:
+            if '.json' in file:
+                name = file.split('.')[0]
+                names.append(name)
+            
+        
+            
+        
+
+if __name__ == "__main__":
+    Manager().get_jsons()
