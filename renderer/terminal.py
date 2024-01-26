e65e163e5d62fa7c0d7c8b1bdda11dfa2e2e3cbf
UnknwnDev <0unknwn.dev@gmail.com>
2024-01-26 10:37:30 -0800

    Simple Terminal Display

--- renderer/terminal.py
+++ renderer/terminal.py
@@ -1,3 +1,4 @@
+from manager.todo import Task
 #!/usr/bin/env python3.11 
 
 class Terminal:
@@ -7,5 +8,17 @@ class Terminal:
     def __format_task(self, task):
         pass
     
-    def printf(self, task):
-        pass
+    def __print_task_info(task: Task):
+        print("="*50)
+        print(f"Category            : {task.category}")
+        print(f"       Title        : {task.title}")
+        print(f"       ID           : {task.id}")
+        print(f"       Descr        : {task.description}")
+        print(f"       Due          : {task.due_date}")
+        print(f"       Remind       : {task.reminder}")
+        print(f"       Done         : {task.completed}")
+        print("="*50)
+    
+    def printf(self, tasks):
+        for task in tasks:
+            self.__print_task_info(task)
