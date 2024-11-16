15bb546d177535d6d4a78957ffa5ec7fd22fa92e
UnknwnDev <0unknwn.dev@gmail.com>
2024-11-16 11:06:21 -0800

    refactor: remove server access

--- run.py
+++ run.py
@@ -1,15 +1,8 @@
-#!/usr/bin/env python3.11 
+#!/usr/bin/env python3.11
 # from manager.manager import Manager
-# from renderer.terminal import Terminal
-from networking.server import JSONSocketServer
-import socket
+from renderer.terminal import Terminal
+
 
 if __name__ == "__main__":
-    
-    HOST = socket.gethostname()  # Change this to your desired host
-    PORT = 5000        # Change this to your desired port
-
-    server = JSONSocketServer(HOST, PORT)
-    server.start()    
-    # term.run()
-
+    term = Terminal()
+    term.run()
